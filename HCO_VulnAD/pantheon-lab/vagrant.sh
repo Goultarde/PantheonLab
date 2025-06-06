5cc64165a1f7b0cadaedd78bf90f33a61c9ed7b3
harouna <harounacoulibaly74@yahoo.com>
2025-06-06 10:52:35 +0200

    windows main.yml

--- HCO_VulnAD/pantheon-lab/vagrant.sh
+++ HCO_VulnAD/pantheon-lab/vagrant.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd vagrant/dc01; vagrant destroy && vagrant up && cd ../../ &&  ansible-playbook -i ansible/inventory/administrator_inventory.yml ansible/playbooks/main.yml
+cd vagrant/dc01; vagrant destroy && vagrant up && cd ../../ &&  ansible-playbook -i ansible/inventory/administrator_inventory.yml ansible/playbooks/windows_main.yml
