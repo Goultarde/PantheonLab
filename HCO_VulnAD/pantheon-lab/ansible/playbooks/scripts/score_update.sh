e14d685eb887c18c61c527e71aeaf62ac6361009
harouna <harounacoulibaly74@yahoo.com>
2025-06-16 03:27:49 +0200

    fix : linux & update windows
    fix de la création du scénario de pivot linux->windows et ajotu d'une tâche planifier pour athena qui execute un shell.ps1 dans C:/coliseum

--- HCO_VulnAD/pantheon-lab/ansible/playbooks/scripts/score_update.sh
+++ HCO_VulnAD/pantheon-lab/ansible/playbooks/scripts/score_update.sh
@@ -1,1 +1,1 @@
-smbclient \\192.168.56.11\scores -u 'smbscores%Sc0r3sS3rv1c3!2024' -put scores.txt
+smbclient //192.168.56.11/scores -U 'smbscores%Sc0r3sS3rv1c3!2024' -c 'put /srv/pantheon.god/scores/scores.txt scores.txt'
